146cf1306a38cc0219ff29c60de6cb943667236c
thannes30 <thannes30@yahoo.com>
2015-09-15 22:05:50 -0400

    added Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,19 @@
 class ArticlesController < ApplicationController
 
   def new
-
   end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:text))
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title,:text)
+    end
 
 end
